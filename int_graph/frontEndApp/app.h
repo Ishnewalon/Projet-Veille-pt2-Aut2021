16ac1d538d787f937e3f112657a93e78f7516247
Stephanie <ishnewalon@hotmail.com>
2021-11-02 13:51:55 -0400

    Added public and private signatures from backend app.h to fe app.h

--- int_graph/frontEndApp/app.h
+++ int_graph/frontEndApp/app.h
@@ -2,12 +2,33 @@
 #define APP_H
 
 #include <QObject>
+#include <pqxx/pqxx>
+#include <string>
+#include "SQLRequests.hpp"
+#include "user.hpp"
 
 class app : public QObject
 {
     Q_OBJECT
 public:
     explicit app(QObject *parent = nullptr);
+    App(): C{SQLRequests::connectionString()} {}
+    void run();
+    void displayConnectionMenu();
+    void connexionEmp();
+    void empNotFound();
+    void billingMenu(User user);
+    void displayBillingOptions();
+    void changePassword();
+    void changePrice();
+
+
+private:    pqxx::connection C;
+    std::string numEmp;
+    std::string mdp;
+    char choixExterne;
+    char choixInterne;
+    SQLRequests *sql{nullptr};
 
 signals:
 
