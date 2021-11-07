2ac6ae85f5894a0bb5015aafbc81a67c67163c0c
Stephanie <ishnewalon@hotmail.com>
2021-11-07 10:52:13 -0500

    Added listRecords from sqlRequests to gui project

--- int_graph/frontEndApp/sqlrequests.cpp
+++ int_graph/frontEndApp/sqlrequests.cpp
@@ -1,6 +1,27 @@
+#include <iostream>
+#include <string>
+#include <pqxx/pqxx>
 #include "sqlrequests.h"
 
 sqlRequests::sqlRequests(QObject *parent) : QObject(parent)
 {
 
+}
+const std::string sqlRequests::dbName {"facturation_resto"};
+const std::string sqlRequests::userName {"ishnewalon"};
+const std::string sqlRequests::password {"inf556"};
+const std::string sqlRequests::hostaddr {"127.0.0.1"};
+const std::string sqlRequests::port {"5432"};
+
+void sqlRequests::listRecords(std::string tableName){
+                std::string sqlStatement = "SELECT * FROM " + tableName + ";";
+                pqxx::nontransaction N(C);
+                pqxx::result R(N.exec(sqlStatement));
+                int counter = 1;
+                for (pqxx::result::const_iterator iterator = R.begin(); iterator != R.end(); ++iterator){ //See what is best ++iterator or iterator++
+
+                        std::cout <<  iterator["nom"].as<std::string>() << std::endl;
+                        counter++;
+                }
+                std::cout << "Fin des donnes" << std::endl;
 }
