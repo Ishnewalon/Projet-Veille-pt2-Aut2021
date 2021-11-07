8913e6a919778d62db28499f35f117ab2e431524
Stephanie <ishnewalon@hotmail.com>
2021-11-07 11:00:54 -0500

    Filled in user.cpp in gui

--- int_graph/frontEndApp/user.cpp
+++ int_graph/frontEndApp/user.cpp
@@ -1,6 +1,39 @@
+#include <iostream>
+#include <string>
+
 #include "user.h"
 
 user::user(QObject *parent) : QObject(parent)
 {
 
+}
+
+std::string user::getNom(){
+        return nom;
+}
+
+std::string user::getPrenom(){
+        return prenom;
+}
+
+std::string user::getNumEmp(){
+        return numEmp;
+}
+
+std::string user::getMDP(){
+        return mdp;
+}
+
+void user::setNom(std::string leNom){
+        nom = leNom;
+}
+
+void user::setPrenom(std::string lePrenom){
+        prenom = lePrenom;
+}
+
+std::string user::toString(){
+        return "Prenom: " + prenom + "\n"
+                "Nom: " + nom + "\n"
+                "Numero d'employe: " + numEmp;
 }
