925129f9130abbf19c9b0d437907be1d226f5110
Stephanie <ishnewalon@hotmail.com>
2021-11-08 10:30:17 -0500

    Added Signals and slots to login.h

--- int_graph/frontEndApp/login.h
+++ int_graph/frontEndApp/login.h
@@ -3,36 +3,47 @@
 
 #include <QObject>
 #include <string>
+#include <QDebug>
 
 class Login : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(std::string empId READ empId WRITE setEmpId NOTIFY empIdChanged)
-    Q_PROPERTY(std::string mdp READ mdp WRITE setMdp NOTIFY mdpChanged)
+
 public:
     explicit Login(QObject *parent = nullptr);
+    Q_PROPERTY(QString empId READ empId WRITE setEmpId NOTIFY empIdChanged)
+    Q_PROPERTY(QString mdp READ mdp WRITE setMdp NOTIFY mdpChanged)
 
-    void setEmpId(std::string empId) {
-        m_empId = empId;
-        emit empIdChanged(empId);
-    }
-    std::string empId() const {
-        return m_empId;
-    }
-    void setMdp(std::string mdp) {
-        m_mdp = mdp;
-        emit mdpChanged(mdp);
-    }
-    std::string mdp() const {
-        return m_mdp;
-    }
-private:
-    std::string m_empId;
-    std::string m_mdp;
+
+       Q_INVOKABLE void printTest() {
+           qDebug()<< m_empId << m_mdp;
+       }
+
+       void setEmpId(QString empId) {
+           m_empId = empId.toStdString();
+           emit empIdChanged(empId.toStdString());
+       }
+
+       std::string empId() const {
+           return m_empId;
+       }
+
+       void setMdp(QString mdp) {
+           m_mdp = mdp.toStdString();
+           emit mdpChanged(mdp.toStdString());
+       }
+
+       std::string mdp() const {
+           return m_mdp;
+       }
+   private:
+       std::string m_empId;
+       std::string m_mdp;
+
 
 signals:
-    void empIdChanged(std::string);
-    void mdpChanged(std::string);
+       void empIdChanged(std::string);
+       void mdpChanged(std::string);
 
 public slots:
     void callMe();
