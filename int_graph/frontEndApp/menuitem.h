b2b8c129682681cc3068695cfd59c75975d94d7f
Stephanie <ishnewalon@hotmail.com>
2021-11-15 09:04:07 -0500

    Added includes and variables to menuItem.h

--- int_graph/frontEndApp/menuitem.h
+++ int_graph/frontEndApp/menuitem.h
@@ -2,12 +2,18 @@
 #define MENUITEM_H
 
 #include <QObject>
+#include <iostream>
+#include <string>
 
 class menuItem : public QObject
 {
     Q_OBJECT
 public:
     explicit menuItem(QObject *parent = nullptr);
+    int id;
+    std::string nom;
+    double prix;
+    std::string menuName;
 
 signals:
 
