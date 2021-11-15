6348f935eeb09a3a7e7100e59968610378535049
Stephanie <ishnewalon@hotmail.com>
2021-11-15 09:10:16 -0500

    Added getters/setters for variables in menuItem

--- int_graph/frontEndApp/menuitem.h
+++ int_graph/frontEndApp/menuitem.h
@@ -10,12 +10,17 @@ class menuItem : public QObject
     Q_OBJECT
 public:
     explicit menuItem(QObject *parent = nullptr);
-
+    std::string getNom();
+    std::string getPrix();
+    std::string getMenuName();
+    void setNom(QString itemName);
+    void setPrix(QString price);
+    void setMenuName(QString menuName);
 
 private:
     int id;
     std::string nom;
-    double prix;
+    std::string prix;
     std::string menuName;
 
 signals:
