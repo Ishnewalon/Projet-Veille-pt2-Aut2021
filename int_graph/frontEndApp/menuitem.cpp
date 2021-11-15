cba4a6cd4fa29a1402a5d1dcffafc02cc2f8c87f
Stephanie <ishnewalon@hotmail.com>
2021-11-15 09:13:30 -0500

    Added getters definition in menuItem.cpp

--- int_graph/frontEndApp/menuitem.cpp
+++ int_graph/frontEndApp/menuitem.cpp
@@ -4,3 +4,18 @@ menuItem::menuItem(QObject *parent) : QObject(parent)
 {
 
 }
+
+std::string menuItem::getNom() {
+    return nom;
+}
+
+std::string menuItem::getPrix() {
+    return prix;
+}
+
+std::string menuItem::getMenuName() {
+    return menuName;
+}
+/*void setNom(QString itemName);
+void setPrix(QString price);
+void setMenuName(QString menuName);*/
