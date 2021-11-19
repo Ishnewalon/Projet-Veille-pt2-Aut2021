ba2c8d9156cda6b0fbffd8ae0e718c6557783578
Stephanie <ishnewalon@hotmail.com>
2021-11-19 12:53:44 -0500

    Added functions in menuItem.cpp

--- int_graph/frontEndApp/menuitem.cpp
+++ int_graph/frontEndApp/menuitem.cpp
@@ -4,3 +4,27 @@ menuItem::menuItem()
 {
 
 }
+
+std::string menuItem::getNom() {
+    return nom;
+}
+
+std::string menuItem::getPrix() {
+    return prix;
+}
+
+std::string menuItem::getNomMenu() {
+    return nomMenu;
+}
+
+void menuItem::setNom(std::string itemName) {
+    nom = itemName;
+}
+
+void menuItem::setPrix(std::string price) {
+    prix = price;
+}
+
+void menuItem::setNomMenu(std::string menuName) {
+    nomMenu = menuName;
+}
