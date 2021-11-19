c8c628a2375159cad9fe85e139a201da3794f649
Stephanie <ishnewalon@hotmail.com>
2021-11-19 12:48:18 -0500

    Recreated menuItem without QObject

--- int_graph/frontEndApp/menuitem.cpp
+++ int_graph/frontEndApp/menuitem.cpp
@@ -1,30 +1,6 @@
 #include "menuitem.h"
 
-menuItem::menuItem(QObject *parent) : QObject(parent)
+menuItem::menuItem()
 {
 
-}
-
-std::string menuItem::getNom() {
-    return nom;
-}
-
-std::string menuItem::getPrix() {
-    return prix;
-}
-
-std::string menuItem::getNomMenu() {
-    return nomMenu;
-}
-
-void menuItem::setNom(std::string itemName) {
-    nom = itemName;
-}
-
-void menuItem::setPrix(std::string price) {
-    prix = price;
-}
-
-void menuItem::setNomMenu(std::string menuName) {
-    nomMenu = menuName;
 }
--- int_graph/frontEndApp/menuitem.h
+++ int_graph/frontEndApp/menuitem.h
@@ -1,30 +1,11 @@
 #ifndef MENUITEM_H
 #define MENUITEM_H
 
-#include <QObject>
-#include <iostream>
-#include <string>
 
-class menuItem : public QObject
+class menuItem
 {
-    Q_OBJECT
 public:
-    explicit menuItem(QObject *parent = nullptr);
-
-    std::string getNom();
-    std::string getPrix();
-    std::string getNomMenu();
-    void setNom(std::string itemName);
-    void setPrix(std::string price);
-    void setNomMenu(std::string menuName);
-
-private:
-    std::string nom;
-    std::string prix;
-    std::string nomMenu;
-
-signals:
-
+    menuItem();
 };
 
 #endif // MENUITEM_H
